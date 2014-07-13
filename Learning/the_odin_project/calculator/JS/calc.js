b5ff7fe9d6272c6cd8448e3f0016b4d9ab22f24f
Mez Hopking <mez@mezhopking.com>
2014-07-13 22:51:56 +1200

    Vowel counter complete (easy OR way).

--- Learning/the_odin_project/calculator/JS/calc.js
+++ Learning/the_odin_project/calculator/JS/calc.js
@@ -1,9 +1,25 @@
 $(document).ready(function($) {
-	console.log(my_max([1,56,2,3,-1,0]))
+	console.log(my_max([1,56,2,3,-1,0]));
+	$('.vowelCount').find('em').text(vowel_count());
 });
 
 
 // Math.max.apply finds largest number in an array. The null (or Math) is required.
-var my_max = function(input) {
+function my_max(input) {
 	return Math.max.apply(null, input);
+};
+
+function vowel_count() {
+	var text = prompt("Paste text to be analysed here:", " "),
+		upperCase = text.toUpperCase(),
+		textSplit = upperCase.split(""),
+		// vowels = ["A", "E", "I", "O", "U"],
+		numVowels = 0;
+
+	for (var i = 0; i < textSplit.length; i++) {
+		if (textSplit[i] == "A" || textSplit[i] == "E" || textSplit[i] == "I" || textSplit[i] == "O" || textSplit[i] == "U") {
+			numVowels++;
+		}
+		return numVowels;
+	}	
 };

58bf87e2a415d86e1ed4b20c5b37c8e453ec5f42
Mez Hopking <mez@mezhopking.com>
2014-07-14 21:49:54 +1200

    Reverse a string function created.

--- Learning/the_odin_project/calculator/JS/calc.js
+++ Learning/the_odin_project/calculator/JS/calc.js
@@ -1,6 +1,7 @@
 $(document).ready(function($) {
-	console.log(my_max([1,56,2,3,-1,0]));
-	$('.vowelCount').find('em').text(vowel_count());
+	// console.log(my_max([1,56,2,3,-1,0]));
+	$('.vowelCount').find('em').text(vowel_count(prompt("How many vowels in this text:", " ")));
+	$(".reverseString").find('em').text(string_reverser(prompt("Text to reverse:", "Here...")));
 });
 
 
@@ -9,17 +10,26 @@ function my_max(input) {
 	return Math.max.apply(null, input);
 };
 
-function vowel_count() {
-	var text = prompt("Paste text to be analysed here:", " "),
+function vowel_count(analyseMe) {
+	var text = analyseMe,
 		upperCase = text.toUpperCase(),
 		textSplit = upperCase.split(""),
 		// vowels = ["A", "E", "I", "O", "U"],
 		numVowels = 0;
-
+		
 	for (var i = 0; i < textSplit.length; i++) {
 		if (textSplit[i] == "A" || textSplit[i] == "E" || textSplit[i] == "I" || textSplit[i] == "O" || textSplit[i] == "U") {
 			numVowels++;
 		}
-		return numVowels;
 	}	
+	return numVowels;
+};
+
+function string_reverser(reverseMe) {
+	var tempArray = [];
+
+	for (var i = 0, strLength = reverseMe.length; i <= strLength; i ++) {
+		tempArray.push(reverseMe.charAt(strLength - i));
+	}
+	return tempArray.join('');
 };
